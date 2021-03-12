d283ad1803578d9b7d687f06ea0f72d7d9971c31
pbgnz <pabgonza1995@gmail.com>
2021-03-11 19:19:04 -0500

    remove depricated fields

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const app = express();
 
 app.get('/api/whoami/', function (req, res) {
     const ipaddress = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
-    req.connection.remoteAddress || 
-    req.socket.remoteAddress || 
-    req.connection.socket.remoteAddress;
+    req.socket.remoteAddress;
     const language = req.headers["accept-language"].split(",")[0];
     const software = req.headers["user-agent"].match(/\(([^)]+)\)/)[1];
     res.send({ipaddress, language, software});
